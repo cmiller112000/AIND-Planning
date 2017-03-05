c14377452eb185904b249845ef5d2bb38d8bf907
Cheryl Miller <cmiller112000@gmail.com>
2017-03-04 21:20:11 -0600

    shell scripts to run tests

---
+++ run_part1a.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+touch part1a_results
+python ./run_search.py -p 1 -s 7 >>part1a_results
+python ./run_search.py -p 2 -s 7 >>part1a_results
+python ./run_search.py -p 3 -s 7 >>part1a_results
---
+++ run_part2.sh
@@ -0,0 +1,10 @@
+touch part2_results
+python ./run_search.py -p 1 -s 8 >>part2_results
+python ./run_search.py -p 2 -s 8 >>part2_results
+python ./run_search.py -p 3 -s 8 >>part2_results
+python ./run_search.py -p 1 -s 9 >>part2_results
+python ./run_search.py -p 2 -s 9 >>part2_results
+python ./run_search.py -p 3 -s 9 >>part2_results
+python ./run_search.py -p 1 -s 10 >>part2_results
+python ./run_search.py -p 2 -s 10 >>part2_results
+python ./run_search.py -p 3 -s 10 >>part2_results
